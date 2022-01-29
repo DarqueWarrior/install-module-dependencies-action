d7c4cee77d399b70972d7cdbdde571f18ce469ca
Donovan Brown <dlb@dlbonline.com>
2022-01-29 13:16:11 -0600

    Added other scripts

---
+++ ConvertTo-NuSpec.ps1
@@ -0,0 +1,21 @@
+[CmdletBinding()]
+param (
+    # version number that should be used when publishing to github
+    [Parameter(Mandatory = $true, ParameterSetName = "PublishGithub")]
+    [version]
+    $Version,
+    
+    # full path to the module psd1 file
+    [Parameter(Mandatory = $true)]
+    [string]
+    $ModulePath
+)
+
+Write-Host "Create NuSpec from PSD1"
+Install-Module -Name Trackyon.Nuget -Scope CurrentUser -Force
+ConvertTo-NuSpec -Path "$ModulePath"
+
+$filename = [System.IO.Path]::GetFileNameWithoutExtension($ModulePath)
+
+Write-Host "Pack module"
+nuget pack "$ModulePath/dist/exdate.nuspec" -NonInteractive -OutputDirectory "$ModulePath/dist" -version $Version -Verbosity Detailed
